68633e84b1ecb5c6152fed7a1cc744bd2b679947
szczypiorofix <Scypiur1>
2018-03-20 08:21:39 +0100

    Dodano obsługę TypeScript.

---
+++ public_html/js/file.js
@@ -0,0 +1,15 @@
+/**
+ * https://code.tutsplus.com/series/typescript-for-beginners--cms-1215
+ */
+var MyClass = /** @class */ (function () {
+    function MyClass(msg) {
+        this.powitanie = msg;
+    }
+    MyClass.prototype.show = function () {
+        console.log(this.powitanie);
+    };
+    return MyClass;
+}());
+var myObj = new MyClass("Siemka!");
+myObj.show();
+//# sourceMappingURL=file.js.map
---
+++ public_html/ts/file.ts
@@ -0,0 +1,19 @@
+/**
+ * https://code.tutsplus.com/series/typescript-for-beginners--cms-1215
+ */
+
+
+ class MyClass {
+     powitanie: string;
+
+     constructor (msg: string) {
+         this.powitanie = msg;
+     }
+
+     show():void {
+         console.log(this.powitanie);
+     }
+ }
+
+ let myObj = new MyClass("Siemka!");
+ myObj.show();
