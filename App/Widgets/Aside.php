35f41a37ef9f71702b622853027085c34ffb523f
szczypiorofix <szczypiorofix@o2.pl>
2018-03-29 14:27:14 +0200

    Dodano widget Twittera do sidebara.

--- App/Widgets/Aside.php
+++ App/Widgets/Aside.php
@@ -9,7 +9,7 @@
 
 namespace Widgets;
 
-use \Core\ModelClasses\Widget;
+use \Core\ModelClasses\Widget, Core\Registry;
 
 /**
  * This is class for aside-type widgets.
@@ -19,6 +19,42 @@ use \Core\ModelClasses\Widget;
 class Aside extends Widget {
     
     public function __construct() {
-        $this->body = '<aside class="sidebar"><p>SIDE BAR...</p></aside>';
+        
+        Registry::add($this);
+        
+        $this->body = '<aside class="sidebar">
+            <p>SIDE BAR...</p>
+            <div class="panel panel-default">
+                <a class="twitter-timeline" href="https://twitter.com/szczypiorofix" data-width="450" data-height="600">
+                    Tweets by @Szczypiorofix
+                </a>
+                <!--  TWITTER FOLLOW BUTTON -->
+                <div style="padding: 5px;">
+                    <a class="twitter-follow-button" href="https://twitter.com/szczypiorofix">Follow @Szczypiorofix</a>
+                </div>
+            </div>
+        </aside>';
+        
+        
+        $twitterJS = '$(document).ready(function(){	
+	window.twttr = (function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0],
+   	t = window.twttr || {};
+	if (d.getElementById(id)) return t;
+ 		js = d.createElement(s);
+ 		js.id = id;
+ 		js.src = "https://platform.twitter.com/widgets.js";
+ 		fjs.parentNode.insertBefore(js, fjs);
+
+ 		t._e = [];
+ 		t.ready = function(f) {
+   		t._e.push(f);
+	};
+	return t;
+	}(document, "script", "twitter-wjs"));
+});';
+        $this->addJS($twitterJS);
     }
+    
+    
 }
